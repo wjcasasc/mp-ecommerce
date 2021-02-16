e7074e29bda67c74d999bfacbcd1a754a96b887d
wjcasasc <wcasasc@gmail.com>
2021-02-16 17:57:52 -0500

    OK

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
 
                                     <button class="as-filter-button" aria-expanded="true" aria-controls="as-search-filters" type="button">
                                         <h2 class=" as-filter-button-text">
-                                            Smartphones
+                                            Smartphones 2222222
                                         </h2>
                                     </button>
 
